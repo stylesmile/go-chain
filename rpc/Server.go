bf453ad58249e0d6b5ac89fb3e25039cf30b1361
chenye <1403539106@qq.com>
2019-05-04 09:24:57 +0800

    http server

---
+++ rpc/Server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"../core"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+var blockchain *core.Blockchain
+
+func run() {
+	// 对外暴露端口
+	http.HandleFunc("/blockchain/get", blockchainGetHandler)
+	http.HandleFunc("/blockchain/write", blockchainWriteHandler)
+	// 监听端口8888
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+func blockchainGetHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, error := json.Marshal(blockchain)
+	if error != nil {
+		http.Error(w, error.Error(), http.StatusInternalServerError)
+		return
+	}
+	// 返回json 字节
+	io.WriteString(w, string(bytes))
+}
+
+func blockchainWriteHandler(w http.ResponseWriter, r *http.Request) {
+	blockData := r.URL.Query().Get("data")
+	blockchain.SendData(blockData)
+	blockchainGetHandler(w, r)
+}
+
+func main() {
+	blockchain = core.NewBlockchain()
+	run()
+}
