da3b94ef155ebaa2b5b84d41d198c8beadfcbecb
chenye <1403539106@qq.com>
2019-05-05 22:52:18 +0800

    模拟数据

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "./core"
 func main() {
 	bc := core.NewBlockchain()
 	bc.SendData("Send 1 BTC to jacky")
-	bc.SendData("Send 1 EOS to jacky")
+	bc.SendData("Send 2 ETC to jacky")
+	bc.SendData("Send 3 ETH to jacky")
+	bc.SendData("Send 4 EOS to jacky")
 	bc.Print()
 }
